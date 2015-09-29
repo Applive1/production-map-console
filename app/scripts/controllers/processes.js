7c18f283e6efaa9ea632fa0eac8954a60a78bb9e
Yehonatan Bitton <bittonye@post.bgu.ac.il>
2015-09-29 10:57:02 +0300

    hotfix: fix process screen

--- app/scripts/controllers/processes.js
+++ app/scripts/controllers/processes.js
@@ -1,26 +1,26 @@
 angular.module('productionMapConsoleApp').controller('ProcessesCtrl', function ($scope, $modalInstance, Processes) {
 
-  var link_id = 1;
-  $scope.process = {
-    id: 0,
-    name: "",
-    description: "",
-    order: 0,
-    default_execution: false
-  };
+    var link_id = 1;
+    $scope.process = {
+        id: 0,
+        name: "",
+        description: "",
+        order: 0,
+        default_execution: false
+    };
 
-  $scope.processes = Processes.all(link_id);
+    $scope.processes = Processes.all(link_id);
 
-  $scope.add_process = function(process){
-    $scope.process = angular.copy(process);
-    Processes.add(link_id, $scope.process);
-  }
+    $scope.add_process = function(process){
+        $scope.process = angular.copy(process);
+        Processes.add(link_id, $scope.process);
+    }
 
-  $scope.delete_process = function(process){
-    Processes.remove(link_id, process);
-  }
+    $scope.delete_process = function(process){
+        Processes.remove(link_id, process);
+    }
 
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
 });
--- app/scripts/services/processes.js
+++ app/scripts/services/processes.js
@@ -8,69 +8,69 @@
  * Factory in the productionMapConsoleApp.
  */
 angular.module('productionMapConsoleApp')
-  .factory('Processes', function () {
-    // Service Logic
+    .factory('Processes', function () {
+        // Service Logic
 
-    var processes = {
-      '1': {
-        p_list: [{
-          name: 'p1',
-          order: 1,
-          description: "bla bla",
-          id: 1
-        },
-        {
-          name: 'p2',
-          order: 2,
-          description: "bla bla",
-          id: 2
-        },
-        {
-          name: 'p3',
-          order: 3,
-          description: "bla bla",
-          id: 3
-        }],
-        size: 4
-      }
-    };
+        var processes = {
+            '1': {
+                p_list: [{
+                    name: 'p1',
+                    order: 1,
+                    description: "bla bla",
+                    id: 1
+                },
+                    {
+                        name: 'p2',
+                        order: 2,
+                        description: "bla bla",
+                        id: 2
+                    },
+                    {
+                        name: 'p3',
+                        order: 3,
+                        description: "bla bla",
+                        id: 3
+                    }],
+                size: 4
+            }
+        };
 
-    function getProcess (link_id, process_id) {
-        var p_list = processes[link_id];
-        var selected_process = {};
-        angular.forEach(function(process){
-          if(process.id === process_id){
-            selected_process = process;
-            return;
-          }
-        });
-        return selected_process;
-    }
-    // Public API here
-    return {
-      get: getProcess,
-      all: function(link_id) {
-        return processes[link_id].p_list;
-      },
-      add: function(link_id, process){
-        if(!processes.hasOwnProperty(link_id)){
-          process.id = 0;
-          processes[link_id] = {
-            p_list: [process],
-            size: 1
-          };
+        function getProcess (link_id, process_id) {
+            var p_list = processes[link_id];
+            var selected_process = {};
+            angular.forEach(function(process){
+                if(process.id === process_id){
+                    selected_process = process;
+                    return;
+                }
+            });
+            return selected_process;
         }
-        else{
-          var link_id_processes = processes[link_id];
-          process.id = link_id_processes.size;
-          link_id_processes.size++;
-          link_id_processes.p_list.push(process);
-        }
-      },
-      remove: function(link_id, process){
-        var p_list = processes[link_id].p_list;
-        var index = p_list.indexOf(process);
-        p_list.splice(index, 1);
-      }
-    };
-  });
+        // Public API here
+        return {
+            get: getProcess,
+            all: function(link_id) {
+                return processes[link_id].p_list;
+            },
+            add: function(link_id, process){
+                if(!processes.hasOwnProperty(link_id)){
+                    process.id = 0;
+                    processes[link_id] = {
+                        p_list: [process],
+                        size: 1
+                    };
+                }
+                else{
+                    var link_id_processes = processes[link_id];
+                    process.id = link_id_processes.size;
+                    link_id_processes.size++;
+                    link_id_processes.p_list.push(process);
+                }
+            },
+            remove: function(link_id, process){
+                var p_list = processes[link_id].p_list;
+                var index = p_list.indexOf(process);
+                p_list.splice(index, 1);
+            }
+        };
+    });
