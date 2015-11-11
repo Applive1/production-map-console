33553130cbbb4a73b8f9ff6f14d4b6e4f2691569
Yehonatan Bitton <bittonye@post.bgu.ac.il>
2015-11-11 22:45:59 +0200

    blocks: support attributes by user

--- app/scripts/controllers/pmblocks.js
+++ app/scripts/controllers/pmblocks.js
@@ -10,11 +10,34 @@
 angular.module('productionMapConsoleApp')
   .controller('PmblocksCtrl',function ($scope, $modalInstance, server) {
   	$scope.server = server;
+  	$scope.attributes = [];
+  	for(var key in server.attributes){
+  		$scope.attributes.push({name: key, value: server.attributes[key]});
+  	};
   	$scope.save = function(){
+  		var attrs = {};
+  		for (var i = $scope.attributes.length - 1; i >= 0; i--) {
+  			var attr = $scope.attributes[i];
+  			attrs[attr.name] = attr.value;
+  		};
+  		$scope.server.attributes = angular.copy(attrs);
+  		console.log($scope.server.attributes);
   		$modalInstance.close($scope.server);
   	}
     $scope.cancel = function () {
         $modalInstance.dismiss('cancel');
     };
+
+    $scope.addAttribute = function() {
+	    $scope.inserted = {
+	    	name: '',
+	    	value: ''
+	    };
+		$scope.attributes.push($scope.inserted);
+	};
+
+	$scope.removeAttribute = function(index) {
+	    $scope.attributes.splice(index, 1);
+	 };
 
   });
