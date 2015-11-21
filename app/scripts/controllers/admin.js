02a97e6d7584abe26a16f26f22aa64838d8eb3b1
Matan Kadosh <kadosh10001@gmail.com>
2015-11-21 18:56:13 +0200

    Add Scheduler view to admin. Make Admin panel a popup, lots of design fixes and adds.

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -7,8 +7,42 @@
  * # AboutCtrl
  * Controller of the productionMapConsoleApp
  */
-angular.module('productionMapConsoleApp').controller('AdminCtrl', ['$scope',function ($scope) {
-
-}]);
+angular.module('productionMapConsoleApp').controller('AdminCtrl', ['$scope', function ($scope) {
+    $scope.view = 1;
+    $scope.calendarView = 'month';
+    $scope.calendar = {
+        calendarDay: new Date(),
+        isCellOpen : true,
+        events: [
+            {
+                title: 'An event',
+                type: 'warning',
+                startsAt: moment().startOf('week').subtract(2, 'days').add(8, 'hours').toDate(),
+                endsAt: moment().startOf('week').add(1, 'week').add(9, 'hours').toDate(),
+                draggable: true,
+                resizable: true
+            },
+            {
+                title: '<i class="glyphicon glyphicon-asterisk"></i> <span class="text-primary">Another event</span>, with a <i>html</i> title',
+                type: 'info',
+                startsAt: moment().subtract(1, 'day').toDate(),
+                endsAt: moment().add(5, 'days').toDate(),
+                draggable: true,
+                resizable: true
+            },
+            {
+                title: 'This is a really long event title that occurs on every year',
+                type: 'important',
+                startsAt: moment().startOf('day').add(7, 'hours').toDate(),
+                endsAt: moment().startOf('day').add(19, 'hours').toDate(),
+                recursOn: 'year',
+                draggable: true,
+                resizable: true
+            }
+        ]
+};
+$scope.eventSources = [];
+}])
+;
 
 
