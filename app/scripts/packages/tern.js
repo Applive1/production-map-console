8e868565dec1cd1cf28b0ec06c7f4f07e3aa0fc3
Yehonatan Bitton <bittonye@post.bgu.ac.il>
2015-12-28 21:32:32 +0200

    support libpm

--- app/scripts/packages/tern.js
+++ app/scripts/packages/tern.js
@@ -45,6 +45,9 @@
 
 'use strict';
 
+var baseURI = "http://localhost:8080";
+
+
 (function(mod) {
   if (typeof exports == "object" && typeof module == "object") // CommonJS
     mod(require("../../lib/codemirror"));
@@ -82,6 +85,11 @@
     this.getHint.async = true;
 
     this.cmScope = false;
+
+    $.get( baseURI + "/libs/lib_production_map.js", function( data ) {
+      self.libPMCode = data;
+      console.log("loaded libpm succesfully");
+    });
   };
 
   CodeMirror.TernServer.prototype = {
@@ -213,7 +221,10 @@
 
   function hint(ts, cm, c) {
     var copiedScope = angular.copy(ts.cmScope);
-    copiedScope.setValue('var map = ' + copiedScope.getValue() + ';');
+    var map_str = "map: " + copiedScope.getValue();
+    var servers_str = "servers: "+ ts.servers;
+    var pm_str = "var pm = { " + map_str + "," + servers_str + "};";
+    copiedScope.setValue('var map = ' + copiedScope.getValue() + ';\n' + ts.libPMCode);
     ts.request(cm, {type: "completions", types: true, docs: true, urls: true}, function(error, data) {
       if (error) return showError(ts, cm, error);
       ts.request(copiedScope, {type: "completions", types: true, docs: true, urls: true}, function(err, scopeData) {
