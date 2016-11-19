9fb4907c1cd4fb33f15503ed4c27ab8ff901fec5
Yehonatan Bitton <lolporer@gmail.com>
2016-11-20 00:34:18 +0200

    servers: add server filter pipe

---
+++ src/app/admin-panel/servers/server-filter.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { ServerFilterPipe } from './server-filter.pipe';
+
+describe('Pipe: ServerFilter', () => {
+  it('create an instance', () => {
+    let pipe = new ServerFilterPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
